4619f97a4e5227cf8581bfff3a1ca6d71d5b2a44
Jayesh Bhole <jayesh@routerprotocol.com>
2024-11-10 02:54:30 +0700

    fix: exponent conversion

--- src/lib/utils/convertToBaseE.ts
+++ src/lib/utils/convertToBaseE.ts
@@ -1,4 +1,5 @@
-export const convertToBaseE = (value: number, decimal: number) => {
+export const convertToBaseE = (value: number | string, decimal: number) => {
+  value = Number(value);
   // Check if the value is a valid number
   if (value === undefined || value === null) return null;
 
@@ -6,20 +7,20 @@ export const convertToBaseE = (value: number, decimal: number) => {
   const exponentialValue = value.toExponential();
 
   // Split the exponential value into mantissa and exponent
-  let [mantissa, exponent] = exponentialValue.split("e");
+  let [mantissa, exponent] = exponentialValue.split('e');
 
   // Provide fallbacks for both mantissa and exponent
-  mantissa = mantissa ?? "1"; // If undefined, use '1' as a default mantissa
-  exponent = exponent ?? "0"; // If undefined, use '0' as a default exponent
+  mantissa = mantissa ?? '1'; // If undefined, use '1' as a default mantissa
+  exponent = exponent ?? '0'; // If undefined, use '0' as a default exponent
 
   // Convert exponent to an integer and add the decimal shift
   const newExponent = parseInt(exponent, 10) + decimal;
 
   // Remove trailing zeros from the mantissa part
-  mantissa = mantissa.replace(/(\.\d*?)0+$/, "$1"); // Remove trailing zeros in mantissa
+  mantissa = mantissa.replace(/(\.\d*?)0+$/, '$1'); // Remove trailing zeros in mantissa
 
   // Remove a decimal point if it's the last character before 'e'
-  mantissa = mantissa.replace(/\.$/, ""); // If there's a trailing '.', remove it
+  mantissa = mantissa.replace(/\.$/, ''); // If there's a trailing '.', remove it
 
   // Return the final result with the adjusted exponent
   return `${mantissa}e${newExponent}`;
