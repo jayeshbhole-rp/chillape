3f6bb5b7c883743228425261e8c18b377ebad999
Jayesh Bhole <jayesh@routerprotocol.com>
2024-11-10 12:44:57 +0700

    chore

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import { Toaster } from '@/components/ui/sonner';
 import AppContext from '@/context/AppContext';
 import type { Metadata } from 'next';
-import { Space_Grotesk } from 'next/font/google';
+import { Tomorrow } from 'next/font/google';
 import '../styles/globals.css';
 
-const spaceGrotesk = Space_Grotesk({ subsets: ['latin'] });
+const spaceGrotesk = Tomorrow({ weight: '500', subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Chill Ape',
